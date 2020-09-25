c72f6648e73603eb311dffe2ef2a0f8a49ea8d18
huangningyuan <huangningyuan029@163.com>
2020-09-25 11:38:01 +0800

    spring数据库事务2

--- day03_spring_account_/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ day03_spring_account_/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -29,61 +29,137 @@ public class AccountServiceImpl implements IAccountService{
     public List<Account> findAllAccount() {
         try {
             //1.开启事务
+            txManger.beginTransaction();
             //2.执行操作
+            List<Account> accounts = accountDao.findAllAccount();
             //3.提交事务
+            txManger.commit();
             //4.返回结果
+            return accounts;
         }catch (Exception e){
             //回滚操作
+            txManger.rollback();
+            throw new RuntimeException(e);
         }finally {
             //6.释放连接
+            txManger.release();
         }
-        return accountDao.findAllAccount();
+
     }
 
     @Override
     public Account findAccountById(Integer accountId) {
         try {
             //1.开启事务
+            txManger.beginTransaction();
             //2.执行操作
+            Account accounts = accountDao.findAccountById(accountId);
             //3.提交事务
+            txManger.commit();
             //4.返回结果
+            return accounts;
         }catch (Exception e){
             //回滚操作
+            txManger.rollback();
+            throw new RuntimeException(e);
         }finally {
             //6.释放连接
+            txManger.release();
         }
-        return accountDao.findAccountById(accountId);
     }
 
     @Override
     public void saveAccount(Account account) {
-        accountDao.saveAccount(account);
+        try {
+            //1.开启事务
+            txManger.beginTransaction();
+            //2.执行操作
+            accountDao.saveAccount(account);
+            //3.提交事务
+            txManger.commit();
+            //4.返回结果
+
+        }catch (Exception e){
+            //回滚操作
+            txManger.rollback();
+            throw new RuntimeException(e);
+        }finally {
+            //6.释放连接
+            txManger.release();
+        }
     }
 
     @Override
     public void updateAccount(Account account) {
-        accountDao.updateAccount(account);
+        try {
+            //1.开启事务
+            txManger.beginTransaction();
+            //2.执行操作
+            accountDao.updateAccount(account);
+            //3.提交事务
+            txManger.commit();
+            //4.返回结果
+
+        }catch (Exception e){
+            //回滚操作
+            txManger.rollback();
+        }finally {
+            //6.释放连接
+            txManger.release();
+        }
     }
 
     @Override
     public void deleteAccount(Integer acccountId) {
-        accountDao.deleteAccount(acccountId);
+        try {
+            //1.开启事务
+            txManger.beginTransaction();
+            //2.执行操作
+           accountDao.deleteAccount(acccountId);
+            //3.提交事务
+            txManger.commit();
+            //4.返回结果
+
+        }catch (Exception e){
+            //回滚操作
+            txManger.rollback();
+        }finally {
+            //6.释放连接
+            txManger.release();
+        }
     }
 
     @Override
     public void transfer(String sourceName, String targetName, Float money) {
-        //1.根据名称查询转出账户
-        Account source= accountDao.findAccountByName(sourceName);
-        //2.根据名称查询转入账户
-        Account target= accountDao.findAccountByName(targetName);
-        //3.转出账户减钱
-        source.setMoney(source.getMoney()-money);
-        //4.转入账户加钱
-        target.setMoney(target.getMoney()+money);
-        //5.更新转出账户
-        accountDao.updateAccount(source);
-        //6.更新转入账户
-        accountDao.updateAccount(target);
+
+
+        try {
+            //1.开启事务
+            txManger.beginTransaction();
+            //2.执行操作
+                //2.1.根据名称查询转出账户
+                Account source= accountDao.findAccountByName(sourceName);
+                //2.2.根据名称查询转入账户
+                Account target= accountDao.findAccountByName(targetName);
+                //2.3.转出账户减钱
+                source.setMoney(source.getMoney()-money);
+                //2.4.转入账户加钱
+                target.setMoney(target.getMoney()+money);
+                //2.5.更新转出账户
+                accountDao.updateAccount(source);
+                //2.6.更新转入账户
+                accountDao.updateAccount(target);
+            //3.提交事务
+            txManger.commit();
+            //4.返回结果
+
+        }catch (Exception e){
+            //回滚操作
+            txManger.rollback();
+        }finally {
+            //6.释放连接
+            txManger.release();
+        }
     }
 
 

596393c14027b71d3738e47adc90263df0d56f22
huangningyuan <huangningyuan029@163.com>
2020-09-27 15:27:23 +0800

    动态代理

---
+++ Spring/day03_spring_proxy/src/main/java/com/itheima/IProducer.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+public interface IProducer {
+
+    /**
+     * .销售
+     * @param money
+     */
+    public void saleProduct(float money);
+
+    /**
+     * 售后
+     * @param money
+     */
+    public void afterService(float money);
+}

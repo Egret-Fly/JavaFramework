a72220499e4137e8e7fa5f71585e5093b13a4347
ningyuanHuang <huangningyuan029@163.com>
2020-09-28 18:57:40 +0800

    通知类型

---
+++ Spring/day03_spring_AOPadvice/src/main/java/com/itheima/Service/impl/AccountServiceImpl.java
@@ -0,0 +1,20 @@
+package com.itheima.Service.impl;
+
+import com.itheima.IAccountService;
+
+public class AccountServiceImpl implements IAccountService {
+    public void saveAccount() {
+        System.out.println("执行了保存");
+    }
+
+    public void updateAccount(int i) {
+        System.out.println("执行了更新:"+i);
+        int j = 1/0;
+
+    }
+
+    public int deleteAccount() {
+        System.out.println("执行了删除");
+        return 0;
+    }
+}
